bc7662143851c968a6492e7ce014f7a5cb66f22f
Steven Aiello <stevenaiello634@yahoo.com>
2023-04-20 17:36:03 -0700

    things dont work yet

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-//bind buttons
+//initalize variables
 let currentNum = [];
 let lastSolved = "";
 let nextInput = "";
 let answer = "";
 
+//bind buttons
 const buttons = document.getElementsByTagName('button');
 const pastWork = document.querySelector('.pastWork');
 const answerBox = document.querySelector('.answer');
 
-//console.log(buttons)
-
+//add listiners
 for (let i = 0; i < buttons.length; i++) {
   const button = buttons[i];
 
@@ -34,7 +34,6 @@ for (let i = 0; i < buttons.length; i++) {
         currentNum.push(input);
         nextInput += input;
     }
-    console.table([ "currentNum = " + currentNum, "lastSolved = " + lastSolved, "nextInput = " + nextInput, "answer = " + answer]);
     updateDisplay();
   });
 
@@ -76,12 +75,27 @@ for (let i = 0; i < buttons.length; i++) {
 
 }
 
+function clear() {
+  currentNum = [];
+  lastSolved = "";
+  nextInput = "";
+  answer = "";
+}
+
+function updateDisplay() {
+  pastWork.innerHTML = `${lastSolved} ${nextInput}`;
+  answerBox.innerHTML = answer;
+}
+
 function solve() {
   if (!currentNum.length) {
     answer = '';
     return;
   }
-  const expression = `${lastSolved} ${nextInput} ${currentNum.join('')}`;
+  let expression = `${lastSolved} ${nextInput}`;
+  if (currentNum.length) {
+    expression += ` ${currentNum.join('')}`;
+  }
   try {
     const result = evaluateThis(expression);
 
@@ -94,49 +108,62 @@ function solve() {
   }
 }
 
-function clear() {
-    currentNum = [];
-    lastSolved = "";
-    nextInput = "";
-    answer = "";
-}
-
-function updateDisplay() {
-    pastWork.innerHTML = `${lastSolved} ${nextInput}`;
-    answerBox.innerHTML = answer;
-}
-
 function evaluateThis(expression) {
   const stack = [];
+  const operatorStack = [];
   const operators = ["+", "-", "*", "/"];
+
+  let currentNumber = "";
 
   for (let i = 0; i < expression.length; i++) {
     const token = expression[i];
 
-    if (!isNaN(token)) {
-      //if the token is a num, push it on to the stack
-      stack.push(parseFloat(token));
+    if (!isNaN(token) || token === ".") {
+      // If the token is a digit or a decimal point, add it to the current number
+      currentNumber += token;
     } else if (operators.includes(token)) {
-      // If the token is an operator, pop the last two numbers off the stack,
-      // perform the operation, and push the result back onto the stack
+      // If the token is an operator, push the current number onto the stack
+      // and reset the current number to an empty string. Then, push the operator
+      // onto the operator stack.
+      if (currentNumber !== "") {
+        stack.push(parseFloat(currentNumber));
+        currentNumber = "";
+      }
+      operatorStack.push(token);
+    } else if (token === " ") {
+      // If the token is a space, ignore it.
+      continue;
+    } else {
+      throw new Error("Invalid token: " + token);
+    }
+
+    if (stack.length >= 2 && operatorStack.length > 0) {
       const b = stack.pop();
       const a = stack.pop();
-
-      switch (token) {
+      const operator = operatorStack.pop();
+      switch (operator) {
+        case "+":
+          stack.push(a + b);
+          break;
+        case "-":
+          stack.push(a - b);
+          break;
         case "*":
-            stack.push(a * b);
-            break;
+          stack.push(a * b);
+          break;
         case "/":
-            stack.push(a / b);
-            break;
-        case '+':
-            stack.push(a + b);
-            break;
-        case '-':
-            stack.push(a - b);
-            break;
+          stack.push(a / b);
+          break;
+        default:
+          throw new Error("Invalid operator: " + operator);
       }
     }
   }
+
+  if (stack.length !== 1 || operatorStack.length !== 0) {
+    throw new Error("Invalid expression.");
+  }
+
   return stack.pop();
 }
+
